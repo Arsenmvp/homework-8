d2367a22a294356f3aae45b50cb13551917bb9bc
Arsenmvp <arsenklaptentko@gmail.com>
2025-06-19 09:36:40 +0300

    Update task1.js

--- task1.js
+++ task1.js
@@ -1,1 +1,6 @@
+function mul(...args) {
+  let numbers = args.filter(arg => typeof arg === "number");
+  if (numbers.length === 0) return 0;
+  return numbers.reduce((acc, num) => acc * num, 1);
+}
 
